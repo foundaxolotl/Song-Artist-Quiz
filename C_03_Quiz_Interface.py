25d43039a714f7d4d0a0c40c5d486cfb34acde04
Lucia <125606190+foundaxolotl@users.noreply.github.com>
2025-05-12 13:29:56 +1200

    Add files via upload

--- C_03_Quiz_Interface.py
+++ C_03_Quiz_Interface.py
@@ -38,18 +38,18 @@ class Play:
     Interface for playing the Colour Quest Game
     """
 
-    def __init__(self, rounds_wanted):
+    def __init__(self, check_rounds):
         self.play_box = Toplevel()
 
-        self.game_frame = Frame(self.play_box)
-        self.game_frame.grid(padx=10, pady=10)
+        self.quiz_frame = Frame(self.play_box)
+        self.quiz_frame.grid(padx=10, pady=10)
 
         # body for most labels...
         body_font = ("Arial", "12")
 
         # list of label details
         play_labels_list = [
-            ["Rounds # of #", ("Arial", "16", "bold"), None, 0],
+            ["Round # of #", ("Arial", "16", "bold"), None, 0],
             ["Score to beat: #", body_font, "#FFF2CC", 1],
             ["Choose a colour below. Good luck.", body_font, "#D5E8D4", 2],
             ["You chose, result", body_font, "#D5E8D4", 4]
@@ -57,7 +57,7 @@ class Play:
 
         play_labels_ref = []
         for item in play_labels_list:
-            self.make_label = Label(self.game_frame, text=item[0], font=item[1],
+            self.make_label = Label(self.quiz_frame, text=item[0], font=item[1],
                                     bg=item[2], wraplength=300, justify="left")
             self.make_label.grid(row=item[3], pady=10, padx=10)
 
@@ -69,27 +69,36 @@ class Play:
         self.results_label = play_labels_ref[3]
 
         # set up colour buttons...
-        self.colour_frame = Frame(self.game_frame)
-        self.colour_frame.grid(row=3)
+        self.artist_frame = Frame(self.quiz_frame)
+        self.artist_frame.grid(row=3)
+        self.year_frame = Frame(self.quiz_frame)
+        self.year_frame.grid(row=5)
 
-        # create four buttons in a 2 x 2 grid
+        # create four buttons for the artist names in a 2 x 2 grid
         for item in range(0, 4):
-            self.colour_button = Button(self.colour_frame, font=("Arial", "12"),
-                                        text="Colour Name", width=15)
-            self.colour_button.grid(row=item // 2,
+            self.artist_button = Button(self.artist_frame, font=("Arial", "12"),
+                                        text="Artist Name", width=15)
+            self.artist_button.grid(row=item // 2,
                                     column=item % 2,
                                     padx=5, pady=5)
+        # create  four buttons for the year names
+        for item in range(0, 4):
+            self.year_button = Button(self.year_frame, font=("Arial", "12"),
+                                      text="Year Name", width=15)
+            self.year_button.grid(row=item // 2,
+                                  column=item % 2,
+                                  padx=5, pady=5)
 
         # Frame to hold hints and stats buttons
-        self.hints_stats_frame = Frame(self.game_frame)
+        self.hints_stats_frame = Frame(self.quiz_frame)
         self.hints_stats_frame.grid(row=6)
 
         # list for buttons (frame | text | bg | command | width | row | column)
         control_button_list = [
-            [self.game_frame, "Next Round", "#0057D8", "", 21, 5, None],
-            [self.hints_stats_frame, "Hints", "#FF8000", "", 10, 0, 0],
-            [self.hints_stats_frame, "Stats", "#333333", "", 10, 0, 1],
-            [self.game_frame, "End", "#990000", self.close_play, 21, 7, None]
+            [self.quiz_frame, "Next Round", "#7FD188", "", 21, 7, None],
+            [self.hints_stats_frame, "Help", "#FFCD93", "", 10, 0, 0],
+            [self.hints_stats_frame, "Stats", "#96AEFF", "", 10, 0, 1],
+            [self.quiz_frame, "End Game", "#990000", self.close_play, 21, 9, None]
 
         ]
 
@@ -98,14 +107,13 @@ class Play:
         for item in control_button_list:
             make_control_button = Button(item[0], text=item[1], bg=item[2],
                                          command=item[3], font=("Arial", "16", "bold"),
-                                         fg="#FFFFFF", width=item[4])
+                                         fg="#000000", width=item[4])
             make_control_button.grid(row=item[5], column=item[6], padx=5, pady=5)
 
             control_ref_list.append(make_control_button)
 
     def close_play(self):
-        # reshow root and end current
-        # game / allow new game to start
+        # closes quiz for a new one to start
         root.deiconify()
         self.play_box.destroy()
 
